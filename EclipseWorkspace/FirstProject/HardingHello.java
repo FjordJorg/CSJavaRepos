e9b6af75ee76eaf14e6ccb474c81a58e9a5453ef
FjordJorg <wstrings92@gmail.com>
2026-01-20 00:07:20 -0700

    java program and Eclipse Git Github tech docs

---
+++ EclipseWorkspace/FirstProject/HardingHello.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class HardingHello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Print hello
+		System.out.print("Hello");
+
+
+	}
+
+}
