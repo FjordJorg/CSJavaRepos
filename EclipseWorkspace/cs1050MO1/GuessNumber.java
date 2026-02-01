33cc59b02c7664a801d4c87df3359a0c5e553058
FjordJorg <wstrings92@gmail.com>
2026-01-31 23:46:17 -0700

    Added Compute area from MO1LO2
    Added compute area from MO1LO2 with comments describing sections of the code.

--- EclipseWorkspace/cs1050MO1/GuessNumber.java
+++ EclipseWorkspace/cs1050MO1/GuessNumber.java
@@ -1,3 +1,4 @@
+
 /**
  * This program takes user input to see if they can guess the 
  * random number generated. Add comments to explain the code.
@@ -5,38 +6,34 @@
 
 import java.util.Scanner;
 
-public class GuessNumber
-{
-	public static void main(String[] args)
-	{		
+//Class name starts with capital letter and follows CamelCase
+public class GuessNumber {
+	// Main method
+	public static void main(String[] args) {
 		final int RANDOM_MULTIPLIER = 100;
 
 		int number = (int) (Math.random() * RANDOM_MULTIPLIER) + 1;
 
 		Scanner keyboardInput = new Scanner(System.in);
-		
+		// Method
 		System.out.println("Guess a magic number between 0 and 100");
 
+		// Statement declaring identifier 'guess' and integer assignment
 		int guess = -1;
-		while (guess != number)
-		{
+		// Statement declaring identifier 'number' with Boolean assignment
+		while (guess != number) {
 
 			System.out.print("\nEnter your guess: ");
 			guess = keyboardInput.nextInt();
 
-			if (guess == number) 
-			{
+			if (guess == number) {
 				System.out.println("Yes, the number is " + number);
-			}
-			else if (guess > number)
-			{
+			} else if (guess > number) {
 				System.out.println("Your guess is too high");
-			}
-			else
-			{
+			} else {
 				System.out.println("Your guess is too low");
 			}
 		}
 
-	}//end of main
-}//end of class
+	}// end of main
+}// end of class
