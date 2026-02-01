99b2d745741ca44ee56e8c98e49d86d0e11d4530
FjordJorg <wstrings92@gmail.com>
2026-02-01 01:17:00 -0700

    Expanded definitions in comments
    Added explanations in comments to ComputeArea and GuessNumber files, broke and fixed code to screenshot for Tech Doc

--- EclipseWorkspace/cs1050MO1/ComputeArea.java
+++ EclipseWorkspace/cs1050MO1/ComputeArea.java
@@ -5,6 +5,11 @@
 /**
  * 
  */
+/*
+ * This program calculates the area of a circle by multiplying pi by radius
+ * squared.
+ */
+// Class name
 public class ComputeArea
 {
 
@@ -14,14 +19,16 @@ public class ComputeArea
 	// Main method
 	public static void main(String[] args)
 	{
-		double radius; // Declare radius as variable
+		// Numeric primitive data types
+		int radius; // Declare radius as variable
 		double area; // Declare area as variable
 
 		// Assign a radius
 		radius = 20;
 
-		// Compute area
-		area = radius * radius * 3.14159;
+		// Compute area, order of operations, operator
+		// Automatic casting: int to double
+		area = radius % radius * 3.14159;
 
 		// Display results
 		System.out.println(
--- EclipseWorkspace/cs1050MO1/GuessNumber.java
+++ EclipseWorkspace/cs1050MO1/GuessNumber.java
@@ -7,11 +7,16 @@
 import java.util.Scanner;
 
 //Class name starts with capital letter and follows CamelCase
-public class GuessNumber {
+public class GuessNumber
+{
 	// Main method
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
+		// Declaring and assigning a constant
 		final int RANDOM_MULTIPLIER = 100;
 
+		// Declaring a variable and assigning an equation applied to user input
+		// Type identifier assignment literal
 		int number = (int) (Math.random() * RANDOM_MULTIPLIER) + 1;
 
 		Scanner keyboardInput = new Scanner(System.in);
@@ -21,16 +26,24 @@ public class GuessNumber {
 		// Statement declaring identifier 'guess' and integer assignment
 		int guess = -1;
 		// Statement declaring identifier 'number' with Boolean assignment
-		while (guess != number) {
+		while (guess != number)
+		{
 
+			// Method with string literal
+			// Standard output with System class
 			System.out.print("\nEnter your guess: ");
 			guess = keyboardInput.nextInt();
 
-			if (guess == number) {
+			// Boolean primitive data type non numeric
+			if (guess == number)
+			{
+				// String is a non primitive data type
 				System.out.println("Yes, the number is " + number);
-			} else if (guess > number) {
+			} else if (guess > number)
+			{
 				System.out.println("Your guess is too high");
-			} else {
+			} else
+			{
 				System.out.println("Your guess is too low");
 			}
 		}
