d599a42ceffcbc55bb4bd7be47c2501a7d8f3bba
FjordJorg <wstrings92@gmail.com>
2026-02-17 13:48:25 -0700

    Updated BMI
    Changed BMI to work, needs more work

--- EclipseWorkspace/cs1050M2/BodyMassIndexActivity.java
+++ EclipseWorkspace/cs1050M2/BodyMassIndexActivity.java
@@ -21,19 +21,16 @@ public class BodyMassIndexActivity
 
 		// Create constants and variables
 		// constants
-		double bmi;
 		// This causes an error because BMI_US_FACTOR has not been initialized
 		// as a value. Find equation, insert below
-		final double BMI_US_FACTOR;
-		double weight = 0;
-		double height = 0;
-		double BMI;
+
 		// factor formula in pounds
 		// Variables
 		// Variables
 		// The user's weight
 		// The user's height
 		// The user's BMI
+		double BMI_US_FACTOR = 703;
 
 		// Create a Scanner object for scanning input from keyboard
 		Scanner input = new Scanner(System.in);
@@ -44,18 +41,19 @@ public class BodyMassIndexActivity
 		// Get the user's weight.
 		System.out.print("Enter your weight, in pounds: ");
 		// ADD CODE to input next double for weight
-		weight = input.nextDouble();
+		double weight = input.nextDouble();
 
 		// Get the user's height.
 		System.out.print("Enter your height, in inches: ");
 		// ADD CODE input next double for height
-		height = input.nextDouble();
+		double height = input.nextDouble();
+
+		double bmi = (weight / (height * height)) * BMI_US_FACTOR;
 
 		// Calculate the user's body mass index.
-		bmi = weight * BMI_US_FACTOR / (height * height);
-
 		// Display the user's BMI.
-		System.out.printf("Your body mass index (BMI) is %f", bmi);
+		// %.2f something like that will limit decimal value
+		System.out.printf("Your body mass index (BMI) is " + %.2fbmi);
 
 	}
 }
