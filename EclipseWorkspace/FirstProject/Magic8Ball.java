dbd2380b4047dc040a3918f8295f46662493b161
FjordJorg <wstrings92@gmail.com>
2026-02-21 00:02:56 -0700

    Added new class files
    For Madlib, Concert prices, Magic 8 ball. No code, just new class files.

---
+++ EclipseWorkspace/FirstProject/Magic8Ball.java
@@ -0,0 +1,39 @@
+public class Magic8Ball
+{
+	public static void main(String[] args)
+	{
+		// Get a random number from 1 to 8
+		int random = (int) ((Math.random() * 8) + 1);
+		// Use if statements to test the random number
+		if (random == 1)
+		{
+			System.out.print("Don't make me sick!");
+		}
+		// and print out 1 of 8 random responses
+		if (random == 2)
+		{
+			System.out.print("Very doubtful!");
+		}
+		if (random == 3)
+		{
+			System.out.print("Probably Not!");
+		}
+		if (random == 4)
+		{
+			System.out.print("Why are you asking me?!");
+		}
+		if (random == 5)
+		{
+			System.out.print("Please don't make me decide!");
+		}
+		if (random == 6)
+		{
+			System.out.print("Fuck no!");
+		}
+		if (random == 7)
+		{
+			System.out.print("Ask again later!");
+		}
+
+	}
+}
---
+++ EclipseWorkspace/cs1050M2/GEMO2Madlib.java
@@ -0,0 +1,24 @@
+/* 
+
+ *  Name: Warren Hirschfeld
+
+ *  Class: CS1050 (T/TH) 
+
+ *  Description: Generates (single) Madlib from user input     
+
+ *    
+
+ */
+public class GEMO2Madlib
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
