a0122ca6af6004f816e28a4c6016e4c26d5bcfb1
FjordJorg <wstrings92@gmail.com>
2026-03-12 13:43:57 -0600

    Created new GEMO4 project
    Added labs from lectures, began work on LabL15ArraysIntro, edited Analyze Numbers to get rid of errors.

---
+++ EclipseWorkspace/CS1050MO4/GEMO4LabL15ArraysIntro.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+public class GEMO4LabL15ArraysIntro
+{
+	public static void main(String[] args)
+	{
+
+		java.util.Scanner input = new java.util.Scanner(System.in);
+		System.out.print("Enter the names of each student: ");
+		String[] firstNames;
+
+		int grades = input.nextInt();
+		double[] numbers = new double[grades]; // Create an array
+		double sum = 0;
+
+	}
+
+}
