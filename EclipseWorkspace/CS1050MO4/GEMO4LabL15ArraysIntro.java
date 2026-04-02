8e133f8ab0aedb4ed3e646e93018f89abad1e3d3
FjordJorg <wstrings92@gmail.com>
2026-04-02 13:55:49 -0600

    Updated Lab15 (GE4 Grades)
    Need to update and submit GE4 Grades from Lab15

--- EclipseWorkspace/CS1050MO4/GEMO4LabL15ArraysIntro.java
+++ EclipseWorkspace/CS1050MO4/GEMO4LabL15ArraysIntro.java
@@ -1,14 +1,36 @@
 /**
  * 
  */
+import java.util.Scanner;
+
 public class GEMO4LabL15ArraysIntro
 {
+	static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args)
 	{
 
-		java.util.Scanner input = new java.util.Scanner(System.in);
+		System.out.print("How many students? ");
+		int amountStudents = input.nextInt();
+		input.nextLine();
+
+		String[] firstNames = new String[amountStudents];
+
 		System.out.print("Enter the names of each student: ");
-		String[] firstNames;
+
+		// for loop to fill array with as many names as inputted
+		for (int i = 0; i < firstNames.length; i++)
+		{
+			System.out.print("Student " + (i + 1) + ": ");
+			firstNames[i] = input.nextLine();
+		}
+
+		// Print names to verify
+		for (int i = 0; i < firstNames.length; i++)
+		{
+			System.out.print("Student " + (i + 1) + ": ");
+			firstNames[i] = input.nextLine();
+		}
 
 		int grades = input.nextInt();
 		double[] numbers = new double[grades]; // Create an array
