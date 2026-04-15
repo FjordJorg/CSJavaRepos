e73c7ff2c702c3922d76288856399bb24b2b8540
FjordJorg <wstrings92@gmail.com>
2026-04-14 23:06:22 -0600

    Added new methods
    inputAthlete data now works and validates, need to fix display and categorize methods.

--- EclipseWorkspace/TeamTrainer/TeamTrainerAppIterationOne.java
+++ EclipseWorkspace/TeamTrainer/TeamTrainerAppIterationOne.java
@@ -21,47 +21,145 @@ public class TeamTrainerAppIterationOne
 	public static void main(String[] args)
 	{
 		// initialize and declare all constants, variables, and strings in main
-		final double BMI_US_FACTOR = 703;
-		int numAthletes = 0;
 		double sumMHR = 0.0;
 		double averageMHR = 0.0;
 		double highestMHR = 0.0;
+		int numAthletes = 0;
+
+		// organizing methods
+		numAthletes = inputNumAthletes(numAthletes);
+
 		String[] athleteName = new String[numAthletes];
 		double[] athleteBMI = new double[numAthletes];
 		String[] athleteBMICategory = new String[numAthletes];
 		double[] maxHeartRate = new double[numAthletes];
 		double[] trainingMHR = new double[numAthletes];
 
-		// organizing methods
-		inputAthleteData(numAthletes, athleteName);
+		inputAthleteData(numAthletes, athleteName, athleteBMI, maxHeartRate);
 
 		// For loop to print out athletes to make sure arrays are stored
 		// properly and names are printed. Delete once code works.
 		for (int index = 0; index < numAthletes; index++)
 		{
-			System.out.printf("\nAthletes names are: " + athleteName[index]);
+			System.out.printf(
+					"\nAthlete " + (index + 1) + " is: " + athleteName[index]);
+			System.out.printf("\nBMI: %.1f", athleteBMI[index]);
+			System.out.printf("\nMHR: " + maxHeartRate[index]);
 		}
 	}
+	public static int inputNumAthletes(int numAthletes)
+	{
 
-	public static void inputAthleteData(int numAthletes, String[] athleteName)
+		do
+		{
+			System.out.println("Enter the number of athletes: ");
+
+			while (!input.hasNextInt())
+			{
+				System.out.print(
+						"Error, please enter a valid number for height in inches: ");
+				input.next();
+			}
+
+			numAthletes = input.nextInt();
+			if (numAthletes <= 0)
+			{
+				System.out.println(
+						" Error, please enter a valid number for height in inches: ");
+			}
+		} while (numAthletes <= 0);
+
+		return numAthletes;
+	}
+	public static void inputAthleteData(int numAthletes, String[] athleteName,
+			double[] athleteBMI, double[] maxHeartRate)
 
 	{
 
-		System.out.println("Enter the number of athletes: ");
-		numAthletes = input.nextInt();
+		input.nextLine(); // clears line for input, otherwise will cause
+		// error
+		int weight = 0;
+		int height = 0;
+		double BMI_US_FACTOR = 703;
+		int age = 0;
 
-		// for loop index = 0 so the arrays track same position while the loop
-		// increments
-		// up to amountStudents
 		for (int index = 0; index < numAthletes; index++)
 		{
-			System.out.println(
-					"Enter the name for athlete " + (index + 1) + ": ");
-			athleteName[index] = input.nextLine();
-			input.nextLine(); // clears line for input, otherwise will cause
-								// error
-		}
 
+			System.out.println(
+					"\nEnter the name for athlete " + (index + 1) + ": ");
+			while (!input.hasNext())
+			{
+				System.out.print("Error, please enter a valid name for athlete "
+						+ (index + 1));
+				input.next();
+			}
+			athleteName[index] = input.nextLine();
+
+			do
+			{
+				System.out.print("Please enter weight in pounds for "
+						+ athleteName[index] + ": ");
+
+				while (!input.hasNextInt())
+				{
+					System.out.print(
+							"Error, please enter a valid number for weight in pounds: ");
+					input.next();
+				}
+
+				weight = input.nextInt();
+				if (weight <= 0)
+				{
+					System.out.println(
+							" Error, please enter a valid number for weight in pounds: ");
+				}
+			} while (weight <= 0);
+
+			do
+			{
+				System.out.print("Please enter height in inches for "
+						+ athleteName[index] + ": ");
+
+				while (!input.hasNextInt())
+				{
+					System.out.print(
+							"Error, please enter a valid number for height in inches: ");
+					input.next();
+				}
+
+				height = input.nextInt();
+				if (height <= 0)
+				{
+					System.out.println(
+							" Error, please enter a valid number for height in inches: ");
+				}
+			} while (height <= 0);
+			athleteBMI[index] = (weight / (double) (height * height))
+					* BMI_US_FACTOR;
+
+			do
+			{
+				System.out.print(
+						"Please enter age for " + athleteName[index] + ": ");
+
+				while (!input.hasNextInt())
+				{
+					System.out.print("Error, number must be greater than 0: ");
+					input.next();
+				}
+
+				age = input.nextInt();
+				if (age <= 0)
+				{
+					System.out
+							.println(" Error, number must be greater than 0: ");
+				}
+			} while (age <= 0);
+			maxHeartRate[index] = 220 - age;
+
+			input.nextLine();
+		}
 		return;
 	}
 }
