16bec79809dd6a0377fd78f44673dbafc232a281
FjordJorg <wstrings92@gmail.com>
2026-04-21 13:50:05 -0600

    added CarDealership class
    Started working on CarDealership, began constructors for Car class.

---
+++ EclipseWorkspace/CS1050MO5/CarDealership.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+
+import java.util.Scanner;
+
+public class CarDealership
+{
+	static Scanner input = new Scanner(System.in);
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		
+		System.out.print(Car);
+		
+		
+	class Car
+	{
+		private String make;
+		private String model;
+		private double price = 0.0;
+		
+	public Car()
+	{
+		
+	}
+		
+	public Car(String make, String model, double price)
+	{
+		
+	}
+	
+	public String getMake(make)
+	{
+		
+	}
+
+	
+	}
+	}
+
+}
