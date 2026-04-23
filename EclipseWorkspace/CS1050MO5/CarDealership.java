657108b4cdefca01f62e24fc49796879b73fca88
FjordJorg <wstrings92@gmail.com>
2026-04-23 13:50:54 -0600

    Began work on Car class
    Added new parameters, constructors.

--- EclipseWorkspace/CS1050MO5/CarDealership.java
+++ EclipseWorkspace/CS1050MO5/CarDealership.java
@@ -2,47 +2,68 @@
  * 
  */
 
-/**
- * 
- */
-
 import java.util.Scanner;
 
 public class CarDealership
 {
 	static Scanner input = new Scanner(System.in);
-	/**
-	 * @param args
-	 */
+
 	public static void main(String[] args)
 	{
-		
-		System.out.print(Car);
-		
-		
-	class Car
-	{
-		private String make;
-		private String model;
-		private double price = 0.0;
-		
-	public Car()
-	{
-		
-	}
-		
-	public Car(String make, String model, double price)
-	{
-		
-	}
-	
-	public String getMake(make)
-	{
-		
-	}
+		System.out.println("Enter make: ");
+		make = input.next();
 
-	
-	}
+		System.out.println("Enter model: ");
+		model = input.next();
+
+		System.out.println("Enter price: ");
+		price = input.nextDouble();
+
+		class Car
+		{
+			private String make;
+			private String model;
+			private double price;
+
+			public Car(String make, String model, double price)
+			{
+				this.make = make;
+				this.model = model;
+				this.price = price;
+
+			}
+
+			public String getMake(String make)
+			{
+				make = input.next();
+				return make;
+			}
+
+			public String getModel(String model)
+			{
+				model = input.next();
+				return model;
+			}
+
+			public static double getPrice(double price)
+			{
+				price = input.nextDouble();
+				return price;
+			}
+
+			public static void getCarDetails(String make, String model,
+					double price, int[] totalCars)
+			{
+
+			}
+
+			public void displayCarDetails()
+			{
+				System.out.printf("\nCar details: " + make + "\n " + model
+						+ "\nPrice: " + price);
+			}
+
+		}
 	}
 
 }
