8e826e0900470bd19f517acc34fcd1f9120f709a
FjordJorg <wstrings92@gmail.com>
2026-04-28 12:16:12 -0600

    Added new class for MO5 TestShapesInheritance
    Copy and pasted from lecture

---
+++ EclipseWorkspace/CS1050MO5/TestShapesInheritance.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class TestShapesInheritance
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
